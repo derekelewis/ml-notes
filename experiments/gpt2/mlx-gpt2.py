037795171119d2b01f2449b551347cb08b7b6ab6
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2024-05-24 07:52:38 -0500

    Remove unneeded context length parameter in MHA

--- experiments/gpt2/mlx-gpt2.py
+++ experiments/gpt2/mlx-gpt2.py
@@ -17,7 +17,7 @@ GPT_CONFIG_124M = {
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+    def __init__(self, d_in, d_out, dropout, num_heads, qkv_bias=False):
         super().__init__()
         assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
         self.d_in = d_in
